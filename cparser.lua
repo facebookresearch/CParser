b20d3d4142d98ac05fd2f7e46b9cdeb2af1b30a3
Leon Bottou <leon@bottou.org>
2015-05-28 15:55:14 -0400

    added quirk for iso646.h

--- cparser.lua
+++ cparser.lua
@@ -64,6 +64,11 @@ knownIncludeQuirks["<threads.h>"] = { -- c11
    "#ifndef thread_local", "# define thread_local _Thread_local", "#endif"
 }
 
+knownIncludeQuirks["<iso646.h>"] = { -- c++
+   "#define and &&", "#define and_eq &=", "#define bitand &", "#define bitor |",
+   "#define compl ~", "#define not !", "#define not_eq !=", "#define or ||",
+   "#define or_eq |=", "#define xor ^", "#define xor_eq ^="
+}
 
 
 ---------------------------------------------------
