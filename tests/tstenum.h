d5a61384df1af2afe1e7784cfd50fd5a2a0ffc57
Leon Bottou <leon@bottou.org>
2019-08-09 14:53:07 -0400

    added copyright notice to prevent scripts from barking.

--- tests/tstenum.h
+++ tests/tstenum.h
@@ -1,3 +1,12 @@
+/* Copyright (c) Facebook, Inc. and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* Note: This test case was originally contributed by 
+   Javier <nanoterrain@yahoo.com> in git commit c6cf0b6fa31
+   with the name tst7.c and licensed under the facebook CLA. */
+
 typedef enum
 {
   ValueOne,
