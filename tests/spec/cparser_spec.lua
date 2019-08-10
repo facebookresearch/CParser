b22d3a2f9e2c18d42f37640922eb3389ae86c158
Leon Bottou <leon@bottou.org>
2019-08-10 17:42:26 -0400

    more copyright notices

--- tests/spec/cparser_spec.lua
+++ tests/spec/cparser_spec.lua
@@ -1,3 +1,12 @@
+-- Copyright (c) Facebook, Inc. and its affiliates.
+-- This source code is licensed under the MIT license found in the
+-- LICENSE file in the root directory of this source tree.
+
+-- Note: This test case was originally contributed by
+-- Javier <nanoterrain@yahoo.com> in git commit c6cf0b6fa31
+-- and licensed under the facebook CLA. */
+
+
 describe('cparser', function()
   local cparser = require('cparser')
 
