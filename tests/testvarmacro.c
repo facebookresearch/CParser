4832aeb37935265219c1f087f9b1d7e7fc42761f
Leon Bottou <leon@bottou.org>
2020-01-07 13:53:49 -0500

    Added copyright in testvarmacro.c

--- tests/testvarmacro.c
+++ tests/testvarmacro.c
@@ -1,4 +1,7 @@
-
+/* Copyright (c) Facebook, Inc. and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #define F_MACRO1(arg1, arg2, ...) \
     do_f(arg1, arg2, __VA_ARGS__)
